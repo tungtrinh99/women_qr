b1732943da9588bbdc368747bb1c9ec0313adfaa
Tung Trinh <tungtrinh99@gmail.com>
2023-03-08 14:58:02 +0700

    fix

--- js/2.js
+++ js/2.js
@@ -1,6 +1,6 @@
 document.onreadystatechange = function () {
     const lady = document.querySelector('.icon-back');
     lady.addEventListener("click", function() {
-        location.href = "1.html";
+        location.href = "index.html";
     })
 }
